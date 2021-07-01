cbc359961e469072973348f1a750d48e686889b4
Sawan Roy <sawan.roy@trunexa.com>
2021-07-01 15:54:39 +0530

    RTC: warings removed and return value updated

--- modules/hardware/scene5-imx8m/RTC/src/rtc.c
+++ modules/hardware/scene5-imx8m/RTC/src/rtc.c
@@ -17,39 +17,48 @@ int set_time(struct rtc_time time)
 {
     int fd;
     fd = open("/dev/rtc0", O_RDWR);
+    
     if (fd < 0)
     {
         return -1;
     }
+    
     retval = ioctl(fd, RTC_SET_TIME, &time);
+    
     if (retval == -1)
     {
         perror("RTC_SET_TIME ioctl");
+         close(fd);
         return -1;
     }
+
     close(fd);
+    return 0;
 }
 
 
 
 int64_t get_time()
 {
-    time_t t_of_day;
+    time_t timeofday;;
     struct rtc_time rtc_tm;
     struct tm t;
-    char buf[80];
     int fd;
     fd = open("/dev/rtc0", O_RDWR);
+   
     if (fd < 0)
     {
         return -1;
     }
     retval = ioctl(fd, RTC_RD_TIME, &rtc_tm);
+    
     if (retval == -1)
     {
         perror("RTC_RD_TIME ioctl");
+        close(fd);
         return -1;
     }
+
     t.tm_year = rtc_tm.tm_year; 
     t.tm_mon = rtc_tm.tm_mon;
     t.tm_mday = rtc_tm.tm_mday;
@@ -57,9 +66,9 @@ int64_t get_time()
     t.tm_min = rtc_tm.tm_min;
     t.tm_sec = rtc_tm.tm_sec;
     t.tm_isdst = -1;
-    t_of_day = mktime(&t);
+    timeofday = mktime(&t);
     close(fd);
-    return t_of_day;
+    return timeofday;
 }
 
 
@@ -68,11 +77,14 @@ int set_Alarm(struct rtc_time rtc_tm)
 {
     int fd;
     fd = open("/dev/rtc0", O_RDWR);
+    
     if (fd < 0)
     {
         return -1;
     }
+    
     retval = ioctl(fd, RTC_ALM_SET, &rtc_tm);
+    
     if (retval == -1)
     {
         if (errno == ENOTTY) 
@@ -83,4 +95,7 @@ int set_Alarm(struct rtc_time rtc_tm)
         perror("RTC_ALM_SET ioctl");
         return -1;
     }
+
+    close(fd);
+    return 0;
 }
