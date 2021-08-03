761e8c950915ab4b0d0612a5df4aaae5672f8496
Nikhil Anjane <nikhil.anjane@trunexa.com>
2021-08-03 12:58:32 +0530

    CAN: read and write function modified

--- modules/hardware/scene5-imx8m/CAN/src/can.c
+++ modules/hardware/scene5-imx8m/CAN/src/can.c
@@ -20,15 +20,46 @@
 #include <sys/socket.h>
 #include <can.h>
 
-int fileDesc;              /**< File descriptor*/
-struct sockaddr_can addr;  /**< transport protocol class address information (e.g. ISOTP) stucture defined in linux/can.h */
-struct can_frame frame; 
+
+
+int fileDesc;              
+struct sockaddr_can addr;
+struct can_frame frame;  
+
+
+
+/*
+    internal functions 
+*/
+bool socket_connect()
+{
+    struct ifreq ifr;
+    
+    if((fileDesc = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) 
+    {
+        perror("Socket");
+        return false;
+    }
+
+    strcpy(ifr.ifr_name, "can0");
+    ioctl(fileDesc, SIOCGIFINDEX, &ifr);
+    memset(&addr, 0, sizeof(addr));
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+    
+    if(bind(fileDesc, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
+    {
+        perror("Bind");
+        return false;
+    }
+
+    return true;
+}
 
 
 
 /*
     Enable and Disable the CAN
-
 */
 bool enable_can(bool state)
 {
@@ -39,7 +70,7 @@ bool enable_can(bool state)
     {
   	    sprintf(cmd,"ifconfig can0 up");
   	    fp = popen(cmd, "r");
-  	    if (fp == NULL) 
+  	    if(fp == NULL) 
         {
     	    return false;
   	    }
@@ -50,7 +81,7 @@ bool enable_can(bool state)
     {
         sprintf(cmd,"ifconfig can0 down");
   	    fp = popen(cmd, "r");
-  	    if (fp == NULL) 
+  	    if(fp == NULL) 
         {
     	    return false;
   	    }
@@ -64,94 +95,51 @@ bool enable_can(bool state)
 /*
     Read data from CAN
 */
-// struct can_frame read_data()
-// {
-//     int nbytes, i;
-//     FILE *fp;
-//     char cmd[20];
-//     printf("foo1_dump_start"); 
-//     sprintf(cmd,"candump can0");
-//   	fp = popen(cmd, "r");
-//   	if (fp == NULL) 
-//     {
-//         perror("close");
-//   	}
-// 	printf("foo1_read");    
-// 	//nbytes = fread(&frame,1000,sizeof(struct can_frame),fp);
-//     printf("foo1");
-//     ////remove this part after final testing ///////////
-// 	while(1)
-//     {
-//         nbytes = fread(&frame,1000,sizeof(struct can_frame),fp);
-//         printf("foo_while");
-//         if(nbytes < 0)
-//         {
-//             perror("Read");
-//             //return 1;
-//         }
-//         printf("0x%03X [%d] ",frame.can_id, frame.can_dlc);
-//         for (i = 0; i < frame.can_dlc; i++)
-//         {
-//             printf("%02X ",frame.data[i]);
-//         }
-//     }
-//     //////////////////////////////////////////////////////////
-// 	pclose(fp); 
-//     return frame;
-// }
-
-struct can_frame read_data() {
-    int nbytes, i;   
+struct can_frame read_data()
+{
+    socket_connect();
+    int nbytes;   
 	nbytes = read(fileDesc, &frame, sizeof(struct can_frame));
-	while(1){
-        if (nbytes < 0) {
-            perror("Read");
-            //return 1;
-        }
-
-        printf("0x%03X [%d] ",frame.can_id, frame.can_dlc);
-
-        for (i = 0; i < frame.can_dlc; i++)
-            printf("%02X ",frame.data[i]);
-
-        printf("\r\n");
+    if(nbytes < 0)
+    {
+        close(fileDesc);
+        perror("read");
     }
-	if (close(fileDesc) < 0) {
+	if(close(fileDesc) < 0)
+    {
 		perror("Close");
-		//return 1;
 	}
     return frame;
 }
 
 
-/*
-    Write data to CAN
-*/
-bool write_data(unsigned int id, int size, char * message)
-{
+
+bool write_data(unsigned int id, int size, char * message) 
+{ 
+    socket_connect();
     frame.can_id = id;
     frame.can_dlc = size;
     sprintf((char *)frame.data, message);
-    FILE *fp;
-    char cmd[20];
-
-    sprintf(cmd,"cansend can0 %d#%s",frame.can_id,frame.data);
-  	fp = popen(cmd, "r");
-  	if (fp == NULL) 
+    
+    if(write(fileDesc, &frame, sizeof(struct can_frame))) 
     {
-        perror("close");
-  	}
-
-    pclose(fp);
+        perror("Write");
+        return false;
+    }
+    
+    if(close(fileDesc) < 0) 
+    {
+        perror("Close");
+        return false;
+    }
+    
     return true;
 }
 
 
 
-
 /*
     Configure the CAN
-
 */
 bool can_configuration(int bitrate)
 {
@@ -180,7 +168,7 @@ bool can_configuration(int bitrate)
 
   	sprintf(cmd,"ip link set can0 type can bitrate %d",speed);
   	fp = popen(cmd, "r");
-  	if (fp == NULL) 
+  	if(fp == NULL) 
     {
         return false;
   	}
